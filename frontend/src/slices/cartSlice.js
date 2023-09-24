0205e8b2707d2a784a60915f5fc2051075089e36
Noseilos <jonaseguiron@gmail.com>
2023-09-24 12:10:03 +0800

    orders api slice and place order screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -36,6 +36,10 @@ const cartSlice = createSlice({
             state.paymentMethod = action.payload;
             return updateCart(state);
         },
+        clearCartItems: (state, action) => {
+            state.cartItems = [];
+            return updateCart(state);
+        }
     }
 });
 
@@ -43,5 +47,8 @@ export const {
     addToCart, 
     removeFromCart,
     saveShippingAddress,
-    savePaymentMethod, } = cartSlice.actions;
+    savePaymentMethod,
+    clearCartItems,
+    
+} = cartSlice.actions;
 export default cartSlice.reducer;
